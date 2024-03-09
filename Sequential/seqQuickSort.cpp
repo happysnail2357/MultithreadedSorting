bfc652aba648ba43e089a8f9d2459de5a6d1efe7
Carl Campbell <carl-campbell-III@yandex.com>
2024-03-09 01:52:28 -0500

    feat: seqQuicksort

--- Sequential/seqQuickSort.cpp
+++ Sequential/seqQuickSort.cpp
@@ -5,9 +5,51 @@
 */
 
 #include "seqSorts.hpp"
+#include <vector>
 
+int partition(std::vector<int32_t>& arr, int low, int high);
+void quickSort(std::vector<int32_t>& arr, int low, int high);
+void seqQuickSort(std::vector<int32_t>* arr);
 
 void seqQuickSort(std::vector<int32_t>* arr)
 {
-	
+	if(arr == nullptr || arr->empty()) 
+	{
+		return;
+	}
+
+	quickSort(*arr,0, arr->size() - 1);
+}
+
+int partition(std::vector<int32_t>& arr, int low, int high)
+{
+	int32_t pivot = arr[high];
+
+	int32_t i = low-1;
+
+	for(int j = low; j <= high - 1; j++) 
+	{
+		if(arr[j] < pivot) 
+		{
+			i += 1;
+			std::swap(arr[i],arr[j]);
+		}
+	}
+	i += 1; 
+	std::swap(arr[i], arr[high]);
+
+	return i;
+}
+
+void quickSort(std::vector<int32_t>& arr, int low, int high)
+{
+	if(low >= high) 
+	{
+		return;
+	}
+
+	int p = partition(arr, low, high);
+
+	quickSort(arr, low, p-1);
+	quickSort(arr, p+1,  high);
 }
